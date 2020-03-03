147358adaff494cc22b04478033b586d3189d9a1
Zach Barbre <zach.barbre@gmail.com>
2020-03-03 13:18:54 -0500

    review page working

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,17 @@ businessModel = require('../models/indexModel');
 router.get('/:business_id?', async function(req, res, next) {
   let businessData = [];
   let reviewsData = [];
+  let partial = '';
   const { business_id } = req.params;
-  reviewsData = await businessModel.getReviewsByBusinessID(business_id);
+  
   console.log('review data', reviewsData);
   if (!!business_id){
     businessData = await businessModel.getBusinessById(business_id);
-    
+    reviewsData = await businessModel.getReviewsByBusinessID(business_id);
+    partial = 'partial-business'
   } else{
     businessData = await businessModel.getAllBusiness();
+    partial = 'partial-index'
   }
   console.log('business data', businessData);
 
@@ -22,10 +25,11 @@ router.get('/:business_id?', async function(req, res, next) {
   res.render('template', 
   {locals:{
     title: 'Resturant Reviews', 
-    businessData: businessData
+    businessData: businessData,
+    reviewsData: reviewsData
   },
     partials:{
-      partial: 'partial-index'
+      partial: partial
     }
   });
 });
