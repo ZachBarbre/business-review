41dfe62a8b2c7f1a586671e8aeb5028f740a8dea
Zach Barbre <zach.barbre@gmail.com>
2020-03-04 13:19:25 -0500

    add post route

--- models/indexModel.js
+++ models/indexModel.js
@@ -19,7 +19,7 @@ class BusinessInfo {
 
     static async getBusinessById(id) {
         try {
-            const response = await db.any(`SELECT * FROM restaurant WHERE restaurant.id = ${id};`);
+            const response = await db.any(`SELECT id, name, category, address FROM restaurant WHERE restaurant.id = ${id};`);
             return response;
         }catch(err){
             console.error(err); 
@@ -28,9 +28,28 @@ class BusinessInfo {
 
     static async getReviewsByBusinessID(id) {
         try {
-            const response = await db.any(`SELECT * FROM review INNER JOIN restaurant ON restaurant.id = review.restaurant_id WHERE restaurant.id = ${id};`);
+            const response = await db.any(`SELECT review.review, review.title, review.stars, reviewer.name FROM review INNER JOIN restaurant ON restaurant.id = review.restaurant_id INNER JOIN reviewer ON reviewer.id = review.reviewer_id WHERE restaurant.id = ${id};`);
             return response;
         } catch (err) {
+            console.error(err);
+        }
+    }
+
+    static async getAverageStarsById(id){
+        try {
+            const response = await db.any(`select AVG(stars) from review WHERE restaurant_id = ${id} GROUP BY stars;`);
+            return response;
+        } catch (err) {
+            console.error(err);
+            
+        }
+    }
+    static async postReviewbyBusinessId(reviewer_id, stars, review, restaurant_id, title){
+        try {
+            const response = await db.one(`INSERT INTO review(reviewer_id, stars, review, restaurant_id, title)
+            VALUES ($1, $2, $3, $4, $5) RETURNING id;`,[reviewer_id, stars, review, restaurant_id, title])
+            return response;
+        } catch (err){
             console.error(err);
         }
     }
--- routes/index.js
+++ routes/index.js
@@ -8,18 +8,26 @@ router.get('/:business_id?', async function(req, res, next) {
   let reviewsData = [];
   let partial = '';
   const { business_id } = req.params;
-  
-  console.log('review data', reviewsData);
+
   if (!!business_id){
     businessData = await businessModel.getBusinessById(business_id);
     reviewsData = await businessModel.getReviewsByBusinessID(business_id);
+    // const starsArray 
     partial = 'partial-business'
+    console.log(reviewsData.map(review => review.stars));
   } else{
     businessData = await businessModel.getAllBusiness();
     partial = 'partial-index'
   }
-  console.log('business data', businessData);
 
+router.post('/', async function(req, res) {
+  const { business_id, review_title, review_text , stars } = req.body;
+  const restIdAsInt = parseInt(business_id);
+  const starsAsInt = parseInt(stars);
+  const postData = await businessModel.postReviewbyBusinessId(3, starsAsInt, review_text, restIdAsInt, review_title) 
+  console.log(postData);
+  res.sendStatus(200);
+})
 
 
   res.render('template', 
